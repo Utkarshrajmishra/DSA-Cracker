63b7063f60a4afd68e5bbdf7aef9f305063e368c
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-04-25 10:06:35 +0530

    Create Find the Duplicate Number.java

---
+++ Arrays/Find the Duplicate Number.java
@@ -0,0 +1,12 @@
+    public int findDuplicate(int[] nums) {
+        HashSet <Integer> ll=new HashSet<Integer>();
+        for(int i=0;i<nums.length;i++)
+        {
+            if(ll.contains(nums[i]))
+            {
+                return nums[i];
+            }
+            ll.add(nums[i]);
+        }
+        return -1;
+    }
