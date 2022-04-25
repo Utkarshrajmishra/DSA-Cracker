9e175d6a6f25219612918758121af8140d99c028
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-04-25 19:02:28 +0530

    Create Floor of a number in a sorted array.java

---
+++ Searching and Sorting/Floor of a number in a sorted array.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class floor {
+    public static void main(String[] args) {
+        Scanner x=new Scanner(System.in);
+        int arr[]={1,3,5,7,8,10};
+        int k=2;
+        int start=0;
+        int end=arr.length-1;
+        if(k<arr[0])
+        {
+            System.out.println("-1");
+        }
+        else {
+            while (start <= end) {
+                int mid = (start + end) / 2;
+                if(arr[mid]<k && arr[mid+1]>k)
+                {
+                    System.out.println(arr[mid]);
+                    break;
+                }
+                else if(arr[mid]==k)
+                {
+                    System.out.println(arr[mid]);
+                    break;
+                }
+                else if(arr[mid]>k)
+                {
+                    end=mid;
+                }
+                else
+                {
+                    start=mid+1;
+                }
+            }
+        }
+
+    }
+}
