451b0392da53db5c30f2eb8af71e91dd5e981e24
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-05-02 08:59:27 +0530

    Create Merge Without Extra Space.java

---
+++ Mathematical/Merge Without Extra Space.java
@@ -0,0 +1,21 @@
+
+class Solution {
+
+    public void merge(int arr1[], int arr2[], int m, int n) {
+        // code here
+         int i = 0, j = 0, k = m - 1;
+        while (i <= k && j < n) {
+            if (arr1[i] < arr2[j])
+                i++;
+            else {
+                int temp = arr2[j];
+                arr2[j] = arr1[k];
+                arr1[k] = temp;
+                j++;
+                k--;
+            }
+        }
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+    }
+}
