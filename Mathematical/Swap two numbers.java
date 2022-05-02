432c9cbad18972633018c1847a56e644c4a2c09b
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-05-02 09:05:32 +0530

    Create Swap two numbers.java

---
+++ Mathematical/Swap two numbers.java
@@ -0,0 +1,12 @@
+class Solution{
+    static List<Integer> get(int a,int b)
+    {
+        // code here
+        List<Integer> ll=new ArrayList<Integer>();
+        
+        ll.add(b);
+        ll.add(a);
+        return ll;
+    
+    }
+}
