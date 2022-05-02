f3e4555d6ba3b022df10527c8a6eb5692114521c
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-05-02 09:11:51 +0530

    Create Anshuman's Favourite Number.cpp

---
+++ Mathematical/Anshuman's Favourite Number.cpp
@@ -0,0 +1,17 @@
+class Solution{   
+public:
+    string isValid(long long N){
+        // code here 
+        string stu=" ";
+        if(N%5==0)
+        {
+            stu="YES";
+        }
+        else
+        {
+            stu="NO";
+        }
+      return stu;
+        
+    }
+};
