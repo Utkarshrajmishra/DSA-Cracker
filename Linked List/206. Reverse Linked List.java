5f3aed28feec06a1ad28afdd8c08f705a9e45ded
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-05-24 07:46:19 +0530

    Create 206. Reverse Linked List.java

---
+++ Linked List/206. Reverse Linked List.java
@@ -0,0 +1,14 @@
+public static Node reverse(Node head)
+    {
+        Node prev = null;
+        Node current = head;
+        Node next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+        return head;
+    }
