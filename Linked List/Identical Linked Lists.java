3286ea2c1c515aea0275ef773c48fb0a7cace16a
Utkarsh Raj Mishra <69050786+Utkarshrajmishra@users.noreply.github.com>
2022-05-27 11:34:25 +0530

    Create Identical Linked Lists.java

---
+++ Linked List/Identical Linked Lists.java
@@ -0,0 +1,15 @@
+class Solution {
+    
+    //Function to check whether two linked lists are identical or not.
+    public boolean isIdentical (Node head1, Node head2){
+        //write your code here 
+        while(head1!=null && head2!=null)
+        {
+            if(head1.data!=head2.data)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
